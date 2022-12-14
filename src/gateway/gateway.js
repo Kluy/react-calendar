f3394864652b491f1b02afa09a650b0329bf0b7b
Kluy <kluy@i.ua>
2022-12-14 21:38:42 +0200

    c5

---
+++ src/gateway/gateway.js
@@ -0,0 +1,38 @@
+const baseUrl = 'https://6390e3db0bf398c73a95f78a.mockapi.io/calendar/events-react/';
+
+export const updateEvent = (taskId, updatedTask) =>
+  fetch(`${baseUrl}${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(updatedTask),
+  })
+    .then(response => response.json())
+    .catch(() => alert('Internal Server Error'));
+
+export const postEvent = newEvent =>
+  fetch(`${baseUrl}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(newEvent),
+  })
+    .then(response => response.json())
+    .catch(() => alert('Internal Server Error'));
+
+export const deleteEvent = taskId =>
+  fetch(`${baseUrl}${taskId}`, { method: 'DELETE' })
+    .then(response => response.json())
+    .catch(() => alert('Internal Server Error'));
+
+export const fetchData = () =>
+  fetch(`${baseUrl}`)
+    .then(response => response.json())
+    .catch(() => alert('Internal Server Error'));
+
+export const fetchEvent = eventId =>
+  fetch(`${baseUrl}${eventId}`)
+    .then(response => response.json())
+    .catch(() => alert('Internal Server Error'));
